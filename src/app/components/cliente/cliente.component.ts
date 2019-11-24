3a230ce1e6361a66d952af909ade8740fa43c715
carlosrodri <carlos.rodriguez12@uptc.edu.co>
2019-11-24 00:11:36 -0500

    modulos de meseros y clientes listos

--- src/app/components/cliente/cliente.component.ts
+++ src/app/components/cliente/cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cliente',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente.component.css']
 })
 export class ClienteComponent implements OnInit {
-
-  constructor() { }
-
+  clientes: any[] = [];
+  url_clientes: string = 'https://lada-f35b2.firebaseio.com/.json';
+  // url_mesas:string='http:····/clientes';
+  constructor(private httpClient: HttpClient) {
+    console.log("service clientes ")
+    this.httpClient.get(this.url_clientes)
+      .subscribe((resp: any) => {
+        this.clientes = resp;
+        console.log(resp);
+      })
+  }
   ngOnInit() {
   }
 
--- src/app/components/mesero/mesero.component.ts
+++ src/app/components/mesero/mesero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-mesero',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mesero.component.css']
 })
 export class MeseroComponent implements OnInit {
-
-  constructor() { }
-
+  meseros: any[] = [];
+  orders: any[] = [];
+  url_meseros: string = 'https://proof-95ace.firebaseio.com/.json';
+  // url_mesas:string='http:····/meseros';
+  constructor(private httpClient: HttpClient) {
+    console.log("service meseros ")
+    this.httpClient.get(this.url_meseros)
+      .subscribe((resp: any) => {
+        this.meseros = resp;
+        getOrders(this.meseros, this.orders);
+        console.log(resp);
+      })
+  }
   ngOnInit() {
   }
 
+}
+
+function getOrders(meseros: any, orders: any) {
+  let i = 0;
+  meseros.forEach(element => {
+    let order = "";
+    element.ordenes.forEach(el => {
+      order += el.nombre+", ";
+    });
+    orders[i] = order;
+    i++;
+  });
 }
