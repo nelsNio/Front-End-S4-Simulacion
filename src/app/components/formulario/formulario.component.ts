3e9304b011a453c21c7465c51f85cf7696e3b1d9
PedroAguirreArias01 <pedro_1995pe@hotmail.com>
2019-11-26 21:16:45 -0500

    Boton iniciar simulacion

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  url_iniciar: string = 'http://localhost:8080/restaurante/simular';
+  // url_mesas:string='http:····/clientes';
+  constructor(private httpClient: HttpClient, private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  iniciarSimulacion(){
+    this.httpClient.get(this.url_iniciar)
+      .subscribe((resp: any) => {
+        
+      });
+      this.router.navigate(['clientes']);
   }
 
 }
