8818972f0ec6703ca326e3a2aa4b347ce8ef4020
GaboH97 <gabriel970826@gmail.com>
2020-02-02 23:27:50 -0500

    .

--- src/app/components/mesa/mesa.component.ts
+++ src/app/components/mesa/mesa.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Mesa } from './Mesa';
 
 @Component({
   selector: 'app-mesa',
@@ -9,6 +10,61 @@ import { HttpClient } from '@angular/common/http';
 export class MesaComponent implements OnInit {
   mesas:any[]= [];
   url_mesas:string='http://localhost:8080//api/mesa/obtenerMesas';
+
+  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  ctx: CanvasRenderingContext2D;
+  listaMesas: Mesa[] = [];
+
+  ngOnInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    for (let i = 0; i < 14; i++) {
+      const mesa = new Mesa(this.ctx);
+      this.listaMesas = this.listaMesas.concat(mesa);
+    }
+    // dibuja primera fila
+    let posicion = 0;
+    for (let z = 0; z < 5; z++) {
+      this.listaMesas[z].draw(75 + posicion, 75);
+      this.dibujarSillas(posicion, 0);
+      posicion += 200;
+    }
+
+    // dibuja segunda fila
+    posicion = 0;
+    for (let z = 5; z < 10; z++) {
+      this.listaMesas[z].draw(75 + posicion, 230);
+      this.dibujarSillas(posicion, 155);
+      posicion += 200;
+    }
+
+    // dibuja tercera fila
+    posicion = 0;
+    for (let z = 10; z < 14; z++) {
+      this.listaMesas[z].draw(75 + posicion, 385);
+      this.dibujarSillas(posicion, 310);
+      posicion += 200;
+    }
+  }
+
+  dibujarSillas(x: number, y: number) {
+    // this.ctx.fillStyle = 'green';
+    // Silla 1
+    this.ctx.fillRect(65 + x, 5 + y, 20, 20);
+
+    // Silla 2
+    this.ctx.fillRect(10 + x, 45 + y, 20, 20);
+
+    // Silla 3
+    this.ctx.fillRect(120 + x, 45 + y, 20, 20);
+
+    // Silla 4
+    this.ctx.fillRect(30 + x, 115 + y, 20, 20);
+
+    // Silla 5
+    this.ctx.fillRect(100 + x, 115 + y, 20, 20);
+  }
+
+
   // url_mesas:string='http:····/mesas';
   constructor(private httpClient: HttpClient) { 
     console.log("service mesas ")
@@ -17,9 +73,6 @@ export class MesaComponent implements OnInit {
         this.mesas=resp;
         console.log(resp);
       })
-  }
-
-  ngOnInit() {
   }
 
 }
